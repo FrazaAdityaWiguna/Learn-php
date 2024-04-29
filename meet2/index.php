b0a2fb16c33d3a9201643850e9034ff19711b640
FrazaAdityaWiguna <adityafraja12@gmail.com>
2024-04-29 20:10:13 +0700

    init

---
+++ meet2/index.php
@@ -0,0 +1,13 @@
+<?php
+  // Meet 2 - PHP Dasar
+  //  Sintaks PHP
+
+  // Standar Output
+  // echo, print
+  // print_r
+  // var_dump
+
+  // echo "Fraza Aditya Wiguna"
+
+  // Penulisan Syntac PHP 
+?>
